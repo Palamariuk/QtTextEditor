8731a6901f197652e638ca16884ffd8020e44cf7
Palamariuk <stolnyimax@gmail.com>
2020-02-24 01:46:10 +0200

    New files

---
+++ TextEditor/mainwindow.cpp
@@ -0,0 +1,87 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_actionExit_triggered()
+{
+    close();
+}
+
+void MainWindow::on_actionNew_triggered()
+{
+    currentFile.clear();
+    ui->textEdit->setText("");
+    windowTitle();
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,"Open some file");
+    QFile file(fileName);
+    currentFile = fileName;
+    if(!file.open(QIODevice::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot open the file: " + file.errorString());
+        return;
+    }
+    setWindowTitle(fileName);
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
+}
+
+
+
+void MainWindow::on_actionSave_triggered()
+{
+    QString fileName = QFileDialog::getSaveFileName(this,"Save as");
+    QFile file(fileName);
+    if(!file.open(QIODevice::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot save the file: " + file.errorString());
+        return;
+    }
+    QTextStream out(&file);
+    currentFile = fileName;
+    setWindowTitle(fileName);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.close();
+}
+
+void MainWindow::on_actionundo_triggered()
+{
+    ui->textEdit->undo();
+}
+
+void MainWindow::on_actionRedo_triggered()
+{
+    ui->textEdit->redo();
+}
+
+void MainWindow::on_actionCopy_triggered()
+{
+    ui->textEdit->copy();
+}
+
+void MainWindow::on_actionPaste_triggered()
+{
+    ui->textEdit->paste();
+}
+
+void MainWindow::on_actionHelp_triggered()
+{
+    QMessageBox::information(this, "Help", "If you have problems, please send me a message on my email: stolnyimax@gmail.com\nMax Palamariuk. NULP. 2020\nTurmalinStudio ");
+}
